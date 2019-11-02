e8ff09cff8e82a81308a82ae71c14f9075812f51
seqre <seqre@disroot.org>
2019-11-02 12:40:18 +0100

    First version of multiple solutions attempt
    Signed-off-by: seqre <seqre@disroot.org>

--- src/main.h
+++ src/main.h
@@ -1,7 +1,12 @@
 #ifndef ADVENTOFCODE_2018_MAIN_H
 #define ADVENTOFCODE_2018_MAIN_H
 
-#include "day_1/src/solution.cpp"
-#include "day_2/src/solution.cpp"
+std::string solutionsExecString(int number) {
+    return ("./days/day_" + std::to_string(number) + "/solution" + std::to_string(number));
+}
+
+void executeSolution(int number) {
+    std::cout << solutionsExecString(number) << std::endl;
+}
 
 #endif //ADVENTOFCODE_2018_MAIN_H
