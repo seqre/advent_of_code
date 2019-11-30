1fb4ebe93c8f726544201bf0f9ffec9541a48488
seqre <seqre@disroot.org>
2019-11-30 17:35:33 +0100

    Changed project structure
    Signed-off-by: seqre <seqre@disroot.org>

--- src/utils/fetch_input.py
+++ src/utils/fetch_input.py
@@ -5,7 +5,7 @@ from os import environ
 
 def get_file(day):
     if 1 <= day <= 24:
-        if not path.exists("day_{}/input.txt".format(day)):
+        if not path.exists("days/day_{}/input.txt".format(day)):
             __download_missing(day)
 
         return __return_existing(day)
@@ -14,7 +14,7 @@ def get_file(day):
 
 
 def __return_existing(day):
-    with open('day_{}/input.txt'.format(day), 'r') as f:
+    with open('days/day_{}/input.txt'.format(day), 'r') as f:
         return f
 
 
@@ -24,7 +24,7 @@ def __download_missing(day):
     response = requests.get('https://adventofcode.com/2018/day/{}/input'.format(day), cookies=session, stream=False)
 
     if response.ok:
-        with open("day_{}/input.txt".format(day), 'wb') as f:
+        with open("days/day_{}/input.txt".format(day), 'wb') as f:
             f.write(response.content)
     else:
         exit('Error during downloading input file')
