d0f4513fd8d0b0491b2cec5abdf36150055a0ae9
seqre <seqre@disroot.org>
2019-11-30 17:31:30 +0100

    Added automatic downloading of input files from the AoC website
    Signed-off-by: seqre <seqre@disroot.org>

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import sys
+
+if not __name__ == "__main__":
+    exit('Not designed to run as module')
+
+print('Hello World!')
+print('My name is:', sys.argv[0])
---
+++ src/utils/fetch_input.py
@@ -0,0 +1,37 @@
+import requests
+from os import path
+from os import environ
+
+
+def get_file(day):
+    if 1 <= day <= 24:
+        if not path.exists("day_{}/input.txt".format(day)):
+            __download_missing(day)
+
+        return __return_existing(day)
+    else:
+        exit('Wrong day number')
+
+
+def __return_existing(day):
+    with open('day_{}/input.txt'.format(day), 'r') as f:
+        return f
+
+
+def __download_missing(day):
+    __check_env()
+    session = {'session': environ['AOC_SESSION']}
+    response = requests.get('https://adventofcode.com/2018/day/{}/input'.format(day), cookies=session, stream=False)
+
+    if response.ok:
+        with open("day_{}/input.txt".format(day), 'wb') as f:
+            f.write(response.content)
+    else:
+        exit('Error during downloading input file')
+
+
+def __check_env():
+    if 'AOC_SESSION' in environ:
+        pass
+    else:
+        exit('Please put your session cookie in AOC_SESSION environmental variable')
