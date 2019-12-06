feaa8958f513384ed88ff33aba18aa8fef341ef8
seqre <seqre@disroot.org>
2019-12-06 07:14:23 +0100

    Day 6 solution
    Signed-off-by: seqre <seqre@disroot.org>

---
+++ src/days/day_6/utils_6.py
@@ -0,0 +1,71 @@
+class Node:
+    def __init__(self, name, parent, level):
+        self.name = name
+        self.parent = parent
+        self.level = level
+        self.children = list()
+
+    def add_child(self, name, parent, level):
+        self.children.append(Node(name, parent, level))
+        return self.children[self.children.__len__() - 1]
+
+
+def parse_input(f):
+    data = dict()
+    for line in f.readlines():
+        temp = str(line).split(')')
+        if temp[0] not in data:
+            data[temp[0]] = list()
+        data[temp[0]].append(temp[1].rstrip())
+
+    return data
+
+
+def create_graph(data, root: Node):
+    if root.name in data:
+        children = data[root.name]
+        for child in children:
+            temp = root.add_child(child, root, root.level + 1)
+            create_graph(data, temp)
+    else:
+        pass
+
+
+def print_graph(root: Node):
+    children = root.children
+    print(root.name, 'with level:', root.level, 'is parent of:', children)
+    for child in children:
+        print_graph(child)
+
+
+def find_node(root: Node, name):
+    for child in root.children:
+        if child.name == name:
+            return child
+
+    for child in root.children:
+        ret = find_node(child, name)
+        if ret is not None:
+            return ret
+    pass
+
+
+def get_parents(root: Node):
+    out = list()
+    temp = root.parent
+
+    while temp is not None:
+        out.append(temp)
+        temp = temp.parent
+
+    return out
+
+
+def find_common_ancestor(santa: Node, you: Node):
+    santa_parents = get_parents(santa)
+    you_parents = get_parents(you)
+
+    for i in range(santa_parents.__len__()):
+        for j in range(you_parents.__len__()):
+            if santa_parents[i] == you_parents[j]:
+                return i + j
--- src/main.py
+++ src/main.py
@@ -3,8 +3,8 @@ from utils import solution_runner
 if not __name__ == "__main__":
     exit('Not designed to run as module')
 
-start = 4
-done_solutions = 4
+start = 6
+done_solutions = 6
 
 for i in range(start, done_solutions + 1):
     print('Day {}'.format(i))
