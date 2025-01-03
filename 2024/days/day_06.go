0e3812fe59d645c545c2f113489d8f61765ac95e
Marek 'seqre' Grzelak <git@seqre.dev>
2025-01-04 00:50:35 +0100

    2024: 6.2 still not working

--- 2024/days/day_06.go
+++ 2024/days/day_06.go
@@ -2,6 +2,8 @@ package days
 
 import (
 	"aoc_2024/utils"
+	"fmt"
+	s "github.com/hashicorp/go-set"
 	"strconv"
 )
 
@@ -145,21 +147,32 @@ func (_ *Day06) rotate(guard Cell) Cell {
 
 type Coord [2]int
 
+func coord(x, y int) Coord { return Coord([]int{x, y}) }
+
 func (d *Day06) Part2() string {
 	possibleLoops := make(map[Coord]struct{}, d.grid.MaxX*d.grid.MaxY)
-
+	pastMoves := make(map[Coord]*s.Set[Cell], d.grid.MaxX*d.grid.MaxY)
+	for x := 0; x < d.grid.MaxX; x++ {
+		for y := 0; y < d.grid.MaxY; y++ {
+			pastMoves[coord(x, y)] = s.New[Cell](4)
+		}
+	}
 	simulated := utils.CopyGrid(d.grid.Grid)
-	pastMoves := utils.InitGrid[Cell](d.grid.MaxX, d.grid.MaxY)
 
 	guardOnMap := true
 	guardX, guardY := d.findGuard()
+	var iteration int
 
 	for guardOnMap {
+		iteration++
+		fmt.Printf("iteration %d: %d %d\n", iteration, guardX, guardY)
 		guard := simulated[guardX][guardY]
 		newX, newY := d.getNextGuardMove(guard, guardX, guardY)
 
-		//fmt.Printf("(%d,%d) -> (%d,%d)\n", guardX, guardY, newX, newY)
-		//fmt.Println(utils.AsStringWithEmphasis(simulated, [2]int{guardX, guardY}))
+		if iteration == -1 {
+			fmt.Printf("(%d,%d) -> (%d,%d)\n", guardX, guardY, newX, newY)
+			fmt.Println(utils.AsStringWithEmphasis(simulated, [2]int{guardX, guardY}))
+		}
 		newPos := d.getCell(simulated, newX, newY)
 
 		switch newPos {
@@ -169,7 +182,7 @@ func (d *Day06) Part2() string {
 				possibleLoops[Coord{newX, newY}] = struct{}{}
 			}
 			simulated[guardX][guardY] = Visited
-			pastMoves[guardX][guardY] = guard
+			pastMoves[coord(guardX, guardY)].Insert(guard)
 			simulated[newX][newY] = guard
 			guardX, guardY = newX, newY
 		case Visited:
@@ -178,10 +191,11 @@ func (d *Day06) Part2() string {
 				possibleLoops[Coord{newX, newY}] = struct{}{}
 			}
 			simulated[guardX][guardY] = Visited
-			pastMoves[guardX][guardY] = guard
+			pastMoves[coord(guardX, guardY)].Insert(guard)
 			simulated[newX][newY] = guard
 			guardX, guardY = newX, newY
 		case Obstacle:
+			pastMoves[coord(guardX, guardY)].Insert(guard)
 			simulated[guardX][guardY] = d.rotate(guard)
 		case Border:
 			simulated[guardX][guardY] = Visited
@@ -195,32 +209,47 @@ func (d *Day06) Part2() string {
 
 // 2028 too high
 
-func (d *Day06) checkLoop(simulated, pastMoves [][]Cell, guard Cell, guardX, guardY int) bool {
-	innerPastMoves := utils.CopyGrid(pastMoves)
+func (d *Day06) checkLoop(simulated [][]Cell, pastMoves map[Coord]*s.Set[Cell], guard Cell, guardX, guardY int) bool {
+	innerPastMoves := make(map[Coord]*s.Set[Cell], d.grid.MaxX*d.grid.MaxY)
+	for x := 0; x < d.grid.MaxX; x++ {
+		for y := 0; y < d.grid.MaxY; y++ {
+			innerPastMoves[coord(x, y)] = pastMoves[coord(x, y)].Copy()
+		}
+	}
+	oldX, oldY := d.getNextGuardMove(guard, guardX, guardY)
+	old := simulated[oldX][oldY]
+	simulated[oldX][oldY] = Obstacle
+
 	rotated := d.rotate(guard)
 	var i int
+	badLoop := guardX == 94 && guardY == 34
 	//fmt.Printf("Starting from %d %d\n", guardX, guardY)
-	//oldX, oldY := d.getNextGuardMove(guard, guardX, guardY)
 
 	for {
+		if badLoop {
+			fmt.Println(utils.AsStringWithEmphasisConstrained(simulated, [2]int{guardX, guardY}, 20, 30))
+		}
 		newX, newY := d.getNextGuardMove(rotated, guardX, guardY)
 
 		next := d.getCell(simulated, newX, newY)
 		if next == Border {
+			simulated[oldX][oldY] = old
 			return false
 		}
 
-		past := innerPastMoves[newX][newY]
-		if past == rotated {
+		past := innerPastMoves[coord(newX, newY)]
+		if past.Contains(rotated) {
 			//fmt.Printf("Found loop at %d %d\n", oldX, oldY)
+			simulated[oldX][oldY] = old
 			return true
 		}
 
 		switch next {
 		case Obstacle:
+			innerPastMoves[coord(newX, newY)].Insert(rotated)
 			rotated = d.rotate(rotated)
 		default:
-			innerPastMoves[guardX][guardY] = rotated
+			innerPastMoves[coord(newX, newY)].Insert(rotated)
 			guardX, guardY = newX, newY
 		}
 		i++
--- 2024/utils/grid.go
+++ 2024/utils/grid.go
@@ -45,20 +45,31 @@ func AsString[T GridType[T]](g [][]T) string {
 	return AsStringWithEmphasis(g, [2]int{})
 }
 func AsStringWithEmphasis[T GridType[T]](g [][]T, emphasis [2]int) string {
+	return asStringWithEmphasis(g, emphasis, 0, len(g), 0, len(g[0]))
+}
+
+func AsStringWithEmphasisConstrained[T GridType[T]](g [][]T, middle [2]int, width, height int) string {
+	halfWidth := (width - 1) / 2
+	halfHeight := (height - 1) / 2
+	x, y := max(middle[0]-halfWidth, 0), max(middle[1]-halfHeight, 0)
+	return asStringWithEmphasis(g, middle, x, width, y, height)
+}
+
+func asStringWithEmphasis[T GridType[T]](g [][]T, emphasis [2]int, startX, width, startY, height int) string {
 	var sb strings.Builder
 	bs, be := "\u001B[1m", "\u001B[0m"
 	bls := "\u001B[1;4;31m"
 	//is := "\u001B[3;4m"
 
 	sb.WriteString("    " + bs)
-	for i := 0; i < len(g); i++ {
+	for i := startX; i < startX+width; i++ {
 		sb.WriteString(fmt.Sprintf("%3d ", i))
 	}
 	sb.WriteString(be + "\n")
 
-	for y := 0; y < len(g[0]); y++ {
+	for y := startY; y < startY+height; y++ {
 		sb.WriteString(bs + fmt.Sprintf("%3d ", y) + be)
-		for x := 0; x < len(g); x++ {
+		for x := startX; x < startX+width; x++ {
 			char := fmt.Sprintf("%3c ", g[x][y].ToRune())
 			if emphasis != [2]int{} && x == emphasis[0] && y == emphasis[1] {
 				char = bls + char + be
